2ba43ea831c1952d7dc354851d177975242baab7
Kateryna Kleshch <ketrins1993@gmail.com>
2022-10-17 13:16:13 +0300

    fix styles of statistics

--- src/components/Statistics/Statistics.styled.js
+++ src/components/Statistics/Statistics.styled.js
@@ -4,4 +4,5 @@ export const Text = styled.p`
   font-size: ${p => p.theme.fontSizes.l};
   color: ${p => p.theme.colors.primaryText};
   margin-bottom: ${p => p.theme.space[4]}px;
+  text-transform: capitalize;
 `;
